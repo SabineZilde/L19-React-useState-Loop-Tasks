6c75fb3bf7704283eccf03e3515c6f8f1d160e48
SabineZilde <sabine.zilde@protonmail.ch>
2021-06-27 19:46:57 +0300

    Task 5

--- src/components/Task5.js
+++ src/components/Task5.js
@@ -11,34 +11,27 @@ For example, this would be a 5x5 table */
 // get it through the map twice
 
 const tableData = [
-                    ['Foo 1-1', 'Foo 1-2', 'Foo 1-3', 'Foo 1-4', 'Foo 1-5'],
-                    ['Foo 2-1', 'Foo 2-2', 'Foo 2-3', 'Foo 2-4', 'Foo 2-5'],
-                    ['Foo 3-1', 'Foo 3-2', 'Foo 3-3', 'Foo 3-4', 'Foo 3-5'],
-                    ['Foo 4-1', 'Foo 4-2', 'Foo 4-3', 'Foo 4-4', 'Foo 4-5'],
-                    ['Foo 5-1', 'Foo 5-2', 'Foo 5-3', 'Foo 5-4', 'Foo 5-5'],
-                ];
+    ['Foo 1-1', 'Foo 1-2', 'Foo 1-3', 'Foo 1-4', 'Foo 1-5'],
+    ['Foo 2-1', 'Foo 2-2', 'Foo 2-3', 'Foo 2-4', 'Foo 2-5'],
+    ['Foo 3-1', 'Foo 3-2', 'Foo 3-3', 'Foo 3-4', 'Foo 3-5'],
+    ['Foo 4-1', 'Foo 4-2', 'Foo 4-3', 'Foo 4-4', 'Foo 4-5'],
+    ['Foo 5-1', 'Foo 5-2', 'Foo 5-3', 'Foo 5-4', 'Foo 5-5'],
+];
 
-const newTable = [];
-const lastTable =[];
+const lastTable = [];
 
 for (let rows of tableData) {
-    newTable.push(rows);
-    for (let columns of newTable) {
-        lastTable.push(<tr><td>{columns}</td></tr>)
+    lastTable.push(<tr></tr>);
+    for (let columns of rows) {
+        lastTable.push(<td>{columns}</td>)
     }
 }
-
-// const rows = tableData.map((row) => {
-//     return <tr><td>{row}</td></tr>;
-// })
-
-
 
 function Task5() {
     return (
         <div>
             <h1>Task 5</h1>
-            <table border="1">{newTable}</table>
+            <table border="1">{lastTable}</table>
         </div>
     )
 }
